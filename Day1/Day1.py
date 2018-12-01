31715c24fe583f2fcbb6e24f4b89e14d8b9af6aa
sdana <seth.dana@gmail.com>
2018-12-01 13:06:46 -0600

    Cleaned up a bit, removed unnecessary code

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,11 +1,5 @@
-import sys
-
 file_path = "./Day1Input.txt"
 input_numbers = open(file_path).read().splitlines()
-
-# print(int(input_numbers))
-
-# input_numbers = [7, 7, -2, -7, -4]
 
 frequency = 0
 calibrate = []
@@ -13,9 +7,7 @@ calibrate = []
 print("Calibrating...")
 for i in range(len(input_numbers)*2):
     for num in input_numbers:
-        # print("current: " + str(frequency) + "input: " + str(num))
         frequency += int(num)
-        # print("result: " + str(frequency))
         if frequency in calibrate:
             print("Found Calibration: ", frequency)
             exit()
